b8ef8c7fef73c4ffe1dd35dfec33067ca97baf23
Pradas137 <pradas137@outlook.com>
2020-10-07 20:09:19 +0200

    first commit

--- calendario.php
+++ calendario.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-	<title>Calendario</title>
-
-<?php
-
-		
-			$month=date("n");
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/index.css">
+    <title>Calendario</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <?php
+               	$month=date("n");
 
 			$year=date("Y");
 
@@ -20,29 +25,33 @@
 
 			$dias=1;
 
-			echo "<table border=1 width=294>";
-
+			echo "<table border=1 width=80>";
 			echo "<tr><th colspan=7> $month</th></tr>";
 
-			echo "<tr> <td width=42>Lunes</td> <td width=42>Martes</td> <td width=42>Miercoles</td> <td width=42>Jueves</td> 
+			echo "<tr> <td width=10>Lunes</td> <td width=42>Martes</td> <td width=42>Miercoles</td> <td width=42>Jueves</td> 
 			<td width=42>Viernes</td> <td width=42>Sabado</td> <td width=42>Domingo</td></tr>";
+            ?>
+        </tr>
+        <?php
 
-
-			for ($contadorSemana=0; $contadorSemana < 5 ; $contadorSemana++) { 
-				echo "<tr></tr>";
-				for ($contadorDia=0; $contadorDia< 7; $contadorDia++) { 
-					if ($dias<$diaSemana || $dia>$ultimoDiaMes) {
-						echo "<td> </td>";
-					}else{
-						echo "<td>$dia</td>";
-						$dia+=1;
-					}
-				$dias+=1;
-				}
-			echo "<td> </td>";
-			}
-	?>
-
-</head>
+        echo "<tr>";
+        $primero = date("w", mktime(0, 0, 0, 1, 1, 2020));
+        $contadorDias = 1;
+        for ($counter = 0; $contador < $primero - 1; $contador++) { 
+            echo "<td></td>";
+            $contadorDias += 1;
+        }
+        for ($dias=1; $dias < 31; $dias++) {
+            echo "<td>";
+            echo "<div>$dias</div>";
+            echo "<div><textarea width=20></textarea></div>";
+            echo "</td>";
+            if ($contadorDias % 7 == 0) {
+                echo "</tr><tr>";
+            }
+            $contadorDias += 1;
+        }
+        ?>
+    </table>
 </body>
 </html>
