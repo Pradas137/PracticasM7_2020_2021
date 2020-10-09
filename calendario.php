67ca6b1e6997883fc8964a8fd673f40c5c6a06f5
Pradas137 <pradas137@outlook.es>
2020-10-09 16:36:05 +0200

    mensage

--- calendario.php
+++ calendario.php
@@ -1,59 +1,58 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="es">
+
 <head>
-    <title>Calendario</title>
-    <style type="text/css">
-        span.day { color: #f00; }
-    </style>
+    <link rel="stylesheet" type="text/css" href="CSS/calendarioCSS.css">
     <?php
-
-        $month=date("F");
-
-        $year=date("Y");
-
-        $diaActual=date("j");
-
-        $diaSemana=date("w");
-
-        $ultimoDiaMes= date("t");
-
-        $dia=1;
-
-        $dias=1;
-
-        echo "<table border=1 width=294>";
-
-        echo "<tr><th colspan=7> $month</th></tr>";
-
-        echo "<tr> 
-                <td width=42>Lunes</td> 
-                <td width=42>Martes</td> 
-                <td width=42>Miercoles</td> 
-                <td width=42>Jueves</td> 
-                <td width=42>Viernes</td>
-                <td width=42>Sabado</td>
-                <td width=42>Domingo</td>
-            </tr>";
-
-            for ($contadorSemana=0; $contadorSemana < 5 ; $contadorSemana++) { 
-                echo "<tr></tr>";
-                for ($contadorDia=0; $contadorDia< 7; $contadorDia++) {
-                    if ($dias<$diaSemana || $dia>$ultimoDiaMes) {
-                        echo "<td> </td>";
-                    }else{
-                        echo "<td>";
-                        echo "<div>$dia</div>";
-                        echo "<div></div>";
-                        echo "</td>";
-                        $dia+=1;
-                    }
-                $dias+=1;
-                }
-            }
+    $month=date("n");
+    $year=date("Y");
+    $diaActual=date("j");
+    $diaSemana=date("w",mktime(0,0,0,$month,1,$year))+7;
+    $ultimoDiaMes=date("d",(mktime(0,0,0,$month+1,1,$year)-1));
+    $meses=array(1=>"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
     ?>
 
+    <title>Ejercicio M7 Calendario</title>
+    <meta charset="utf-8">
 </head>
 <body>
+    <h1>Calendario 2020 - 2021 Adrian</h1>
+    <table id="calendario">
+        <caption><?php echo $meses[$month]." ".$year?></caption>
+        <tr>
+            <th>Lun</th><th>Mar</th><th>Mie</th><th>Jue</th>
+            <th>Vie</th><th>Sab</th><th>Dom</th>
+        </tr>
+
+        <tr bgcolor="silver">
+            <?php
+
+            $last_cell=$diaSemana+$ultimoDiaMes;
+            for($i=1;$i<=42;$i++){
+                if($i==$diaSemana){
+                    $day=1;
+                }
+                if($i<$diaSemana || $i>=$last_cell){
+                    echo "<td>&nbsp;</td>";
+                }else{
+                    if($day==$diaActual)
+                        echo "<td class='hoy'>$day</td>";
+                    else{
+                        echo "<td>$day</td>";
+                    }
+                    $day++;
+                }
+                if($i%7==0){
+                    echo "</tr><tr>\n";
+                }
+            }
+        ?>
+
+    </tr>
+
+</table>
 
 </body>
+
 </html>
