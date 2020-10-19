56580df5a292d89b985ccbe7a355245a59c9ed7e
Pradas137 <pradas137@outlook.es>
2020-10-19 19:11:12 +0200

    mejora carrito y pedido

--- Botiga/Carrito.php
+++ Botiga/Carrito.php
@@ -5,46 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart</title>
     <link rel="stylesheet" href="CSS/Botiga.css">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <script src="https://kit.fontawesome.com/b17b075250.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
     <?php
     session_start();
     if (!isset($_SESSION["Carrito"])) {
         $_SESSION["Carrito"] = [];
     }
-    $file_lines = file("productes.txt");
-    $catalogue = [];
-    foreach ($file_lines as $key => $file_line) {
-        $product = explode(';', $file_line);
-        array_push($product, $key);
-        array_push($catalogue, $product);
+    $fichero= file("productes.txt");
+    $catalogo = [];
+    foreach ($fichero as $key => $fichero) {
+        $producto= explode(';', $fichero);
+        array_push($producto, $key);
+        array_push($catalogo, $producto);
     }
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        if (isset($_GET['productId'], $_GET['productQty'])) {
-            $product = $catalogue[$_GET['productId']];
-            array_push($product, $_GET['productQty']);
-            array_push($_SESSION["Carrito"], $product );
+        if (isset($_GET['producto'], $_GET['cantidad'])) {
+            $producto = $catalogo[$_GET['producto']];
+            array_push($producto, $_GET['cantidad']);
+            array_push($_SESSION["Carrito"], $producto );
         }
         if (isset($_GET['line'])) {
             unset($_SESSION["Carrito"][$_GET['line']]);
         }
     }
     ?>
+
+
     <div>
         <table id="tabla">
             <h1>Mostrar Nuevo Producto</h1>
                 <th>Producto</th>
                 <th>Precio</th>
                 <th>Cantidad</th>
-                <th>Actions</th>
+                <th>Eliminar</th>
                 <?php
-                    foreach ($_SESSION["Carrito"] as $key => $product) {
+                    foreach ($_SESSION["Carrito"] as $key => $producto) {
                         echo "<tr>";
-                        echo "<td>$product[0]</td>";
-                        echo "<td>", number_format((float)$product[2], 2, ",", " ") . " €", "</td>";
-                        echo "<td>$product[4]</td>";
+                        echo "<td>$producto[0]</td>";
+                        echo "<td>", number_format((float)$producto[2], 2, ",", " ") . " €", "</td>";
+                        echo "<td>$producto[4]</td>";
                         echo "<td><a class=\"btn btn-danger\" href=Pedido.php?line=$key><span class=\"fas fa-trash\"></span> Delete</a></td>";
                         echo "</tr>";
                     }
@@ -52,22 +53,23 @@
                 <tr>
                     <form action="Pedido.php" method="get">
                         <td colspan="2">            
-                            <select class="form-control" id="Producto" name="ProductoId">
+                            <select class="form-control" id="selectProduct" name="producto">
                                 <?php
-                                foreach ($catalogue as $key => $product) {
-                                    echo "<option value=$key>$product[0] | $product[2]€/Unidad</option>";
+                                foreach ($catalogo as $key => $producto) {
+                                    echo "<option value=$key>$producto[0] | $producto[2]€</option>";
                                 }
                                 ?>
                             </select>                        
                         </td>
                         <td>            
-                            <input type="number" class="form-control" id="productQty" placeholder="1" name="productQty" step="1" required>                   
+                            <input type="number" class="form-control" id="cantidad" placeholder="1" name="cantidad" step="1" required>                   
                         </td>
                         <td>
-                            <button type="submit" class="btn btn-primary"><span class="fas fa-plus"></span> Añadir</button> 
+                            <a href="Pedido.php" id="enlace">Añadir</a>
                         </td> 
                     </form>
-                </tr> 
+                </tr>
+            </tbody> 
         </table> 
     </div> 
 
--- Botiga/Pedido.php
+++ Botiga/Pedido.php
@@ -3,46 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping Cart</title>
+    <title>Carrito</title>
     <link rel="stylesheet" href="CSS/Botiga.css">
 </head>
 <body>
+
     <?php
     session_start();
     if (!isset($_SESSION["Carrito"])) {
         $_SESSION["Carrito"] = [];
     }
-    $file_lines = file("productes.txt");
-    $catalogue = [];
-    foreach ($file_lines as $key => $file_line) {
-        $product = explode(';', $file_line);
-        array_push($product, $key);
-        array_push($catalogue, $product);
+    $fichero= file("productes.txt");
+    $catalogo = [];
+    foreach ($fichero as $key => $fichero) {
+        $producto= explode(';', $fichero);
+        array_push($producto, $key);
+        array_push($catalogo, $producto);
     }
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         if (isset($_GET['producto'], $_GET['cantidad'])) {
-            $product = $catalogue[$_GET['producto']];
-            array_push($product, $_GET['cantidad']);
-            array_push($_SESSION["Carrito"], $product );
+            $producto = $catalogo[$_GET['producto']];
+            array_push($producto, $_GET['cantidad']);
+            array_push($_SESSION["Carrito"], $producto );
         }
         if (isset($_GET['line'])) {
             unset($_SESSION["Carrito"][$_GET['line']]);
         }
     }
     ?>
+
+
     <div>
-        <table id="tabla">
+        <table id="tabla" clas="color">
             <h1>Mostrar Nuevo Producto</h1>
                 <th>Producto</th>
                 <th>Precio</th>
                 <th>Cantidad</th>
                 <th>Eliminar</th>
                 <?php
-                    foreach ($_SESSION["Carrito"] as $key => $product) {
+                    foreach ($_SESSION["Carrito"] as $key => $producto) {
                         echo "<tr>";
-                        echo "<td>$product[0]</td>";
-                        echo "<td>", number_format((float)$product[2], 2, ",", " ") . " €", "</td>";
-                        echo "<td>$product[4]</td>";
+                        echo "<td>$producto[0]</td>";
+                        echo "<td>", number_format((float)$producto[2], 2, ",", " ") . " €", "</td>";
+                        echo "<td>$producto[4]</td>";
                         echo "<td><a class=\"btn btn-danger\" href=Pedido.php?line=$key><span class=\"fas fa-trash\"></span> Delete</a></td>";
                         echo "</tr>";
                     }
@@ -52,8 +55,8 @@
                         <td colspan="2">            
                             <select class="form-control" id="selectProduct" name="producto">
                                 <?php
-                                foreach ($catalogue as $key => $product) {
-                                    echo "<option value=$key>$product[0] | $product[2]€/Unidad</option>";
+                                foreach ($catalogo as $key => $producto) {
+                                    echo "<option value=$key>$producto[0] | $producto[2]€</option>";
                                 }
                                 ?>
                             </select>                        
@@ -62,7 +65,7 @@
                             <input type="number" class="form-control" id="cantidad" placeholder="1" name="cantidad" step="1" required>                   
                         </td>
                         <td>
-                            <button type="submit" class="btn btn-primary"><span class="fas fa-plus"></span> Add</button> 
+                            <a href="Pedido.php" id="enlace">Añadir</a>
                         </td> 
                     </form>
                 </tr>
